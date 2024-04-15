bc1eb1bce68b49a3ae61e2e8acf96140fb5a8027
FuriousGopher <98763500+FuriousGopher@users.noreply.github.com>
2024-04-15 19:17:14 +0200

    new fun to get all customer info separated

--- src/repositories/customer.repository.ts
+++ src/repositories/customer.repository.ts
@@ -41,11 +41,67 @@ export class CustomerRepository {
   }
 
   static async findAllInfo(customerId: number) {
-    return await Customer.createQueryBuilder('customer')
+    const customers = await Customer.createQueryBuilder('customer')
       .leftJoinAndSelect('customer.sites', 'site')
       .leftJoinAndSelect('site.meters', 'meter')
       .leftJoinAndSelect('meter.circuits', 'circuit')
       .where('customer.id = :customerId', { customerId })
+      .andWhere('customer.deleted_at IS NULL')
+      .andWhere('site.deleted_at IS NULL')
+      .andWhere('meter.deleted_at IS NULL')
+      .andWhere('circuit.deleted_at IS NULL')
       .getMany();
+
+    const customerInfo = customers.map((customer) => ({
+      id: customer.id,
+      name: customer.name,
+      email: customer.email,
+      vat_number: customer.vat_number,
+      createdAt: customer.createdAt,
+    }));
+
+    const allSites = customers.flatMap((customer) =>
+      customer.sites.map((site) => ({
+        id: site.id,
+        name: site.name,
+        coordinates: site.coordinates,
+        address: site.address,
+        post_code: site.post_code,
+        createdAt: site.createdAt,
+      })),
+    );
+
+    const allMeters = customers.flatMap((customer) =>
+      customer.sites.flatMap((site) =>
+        site.meters.map((meter) => ({
+          id: meter.id,
+          name: meter.name,
+          serialNumber: meter.serialNumber,
+          installationDate: meter.installationDate,
+          createdAt: meter.createdAt,
+        })),
+      ),
+    );
+
+    const allCircuits = customers.flatMap((customer) =>
+      customer.sites.flatMap((site) =>
+        site.meters.flatMap((meter) =>
+          meter.circuits.map((circuit) => ({
+            id: circuit.id,
+            name: circuit.name,
+            is_main: circuit.is_main,
+            installationDate: circuit.installationDate,
+            createdAt: circuit.createdAt,
+          })),
+        ),
+      ),
+    );
+
+    return {
+      customerInfo,
+      allSites,
+      allMeters,
+      allCircuits,
+    };
   }
 }

2979980cf1ce5ba33496152aaf5690a6319eef2d
FuriousGopher <alkazavr94@gmail.com>
2024-04-15 15:09:57 +0200

    add validation to create new agent

--- src/router/routes/admin.router.ts
+++ src/router/routes/admin.router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { AdminController } from '../../controllers/admin.controller';
+import {emailPasswordValidator} from "../../validators/emailLoginValidatorInput";
+import {validationMiddleware} from "../../validators/validationErorrsMiddleware";
 
 export const adminRouter = Router();
 
-adminRouter.post('/create-new-admin', AdminController.createNewAdmin);
+adminRouter.post('/create-new-admin',emailPasswordValidator,validationMiddleware, AdminController.createNewAdmin);
 
 adminRouter.post('/delete-admin', AdminController.deleteAdmin);
